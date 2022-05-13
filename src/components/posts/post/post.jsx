1ec597a2e168cd57bef482dfe5855d938e863dbe
chenfengqi <chenfengqi_i@didiglobal.com>
2022-05-13 16:05:45 +0800

    修改PF

--- src/components/posts/post/post.jsx
+++ src/components/posts/post/post.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import './post.css';
 
 export default function post({post}) { 
-  const PF = "http://localhost:5000/images/";
+  const PF = "http://127.0.0.1:5000/images/";
 
   return (
     <div className="post">
